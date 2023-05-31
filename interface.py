6ff60b375001d29405029bef5e9c317ac355957b
DESKTOP-GKSCSJ1\Paula <paulamrna.01@gmail.com>
2023-06-01 02:05:33 +0300

    - datebase

--- interface.py
+++ interface.py
@@ -1,11 +1,9 @@
 import requests
 import tkinter as tk
 
-# Define the host address of the sound level meter
 host = "http://10.100.38.87"
 
 def start_measurement():
-    # Send a POST request to start the measurement
     response = requests.post(host + "/webxi/applications/slm/start")
     if response.status_code == 200:
         result_label["text"] = "Measurement started successfully."
@@ -13,16 +11,14 @@ def start_measurement():
         result_label["text"] = "Failed to start the measurement."
 
 def read_values():
-    # Send a GET request to retrieve the sound level values
     response = requests.get(host + "/webxi/applications/slm/outputs/laf")
     if response.status_code == 200:
-        sound_level = response.json() / 100  # Convert the value to dB
+        sound_level = response.json() / 100 
         result_label["text"] = "Sound Level: " + str(sound_level)
     else:
         result_label["text"] = "Failed to read the sound level values."
 
 def stop_measurement():
-    # Send a POST request to stop the measurement
     response = requests.post(host + "/webxi/applications/slm/stop")
     if response.status_code == 200:
         result_label["text"] = "Measurement stopped successfully."
@@ -32,15 +28,9 @@ def stop_measurement():
 def on_quit():
     root.destroy()
 
-# Create the main GUI window
 root = tk.Tk()
 root.title("Sound Level Meter Interface")
 
-# Create the main GUI window
-root = tk.Tk()
-root.title("Sound Level Meter Interface")
-
-# Create GUI elements
 menu_label = tk.Label(root, text="Menu:", font=("Arial", 16, "bold"))
 menu_label.pack(pady=10)
 
@@ -59,10 +49,9 @@ result_label.pack(pady=10)
 quit_button = tk.Button(root, text="Quit", command=on_quit, font=("Arial", 14))
 quit_button.pack(pady=5)
 
-# Configure window appearance
+
 root.configure(bg="#f0f0f0")
 root.geometry("300x300")
 root.resizable(False, False)
 
-# Start the GUI event loop
 root.mainloop()
--- sound level meter.py
+++ sound level meter.py
@@ -1,36 +1,62 @@
 import requests
+import mysql.connector
+import time
 
-# Define the host address of the sound level meter
 host = "http://10.100.38.87"
 
+mydb = mysql.connector.connect(
+  host="sql8.freemysqlhosting.net",
+  user="sql8622787",
+  password="YkGsqm5q7T",
+  database="sql8622787"
+)
+
+cursor = mydb.cursor()
+
+for element in cursor:
+  print(element)
+
+sql = "INSERT INTO masuratori_sonometru (ID, TIMESTAMP, VALUE) VALUES (%s, %s)"
+
+formatted_sql = sql.format(id_value, timestamp_value, value_value)
+
+formatted_sql = f"INSERT INTO masuratori_sonometru (ID, TIMESTAMP, VALUE) VALUES ({id_value}, {timestamp_value}, {value_value})"
+cursor.execute(formatted_sql)
+mydb.commit()
+
+print(cursor.rowcount, "row(s) were inserted.")
+
 def start_measurement():
-    # Send a POST request to start the measurement
-    response = requests.post(host + "/webxi/applications/slm/start")
+    response = requests.post(host + "/webxi/applications/SLM/start?password=xxxxxxx")
     if response.status_code == 200:
         print("Measurement started successfully.")
     else:
         print("Failed to start the measurement.")
+        print(response.status_code)
+    time.sleep(2)
 
 def read_values():
-    # Send a GET request to retrieve the sound level values
-    response = requests.get(host + "/webxi/applications/slm/outputs/laf")
+    response = requests.get(host + "/webxi/applications/SLM/outputs/laf?password=xxxxxxx")
     if response.status_code == 200:
-        sound_level = response.json() / 100  # Convert the value to dB
+        sound_level = response.json() / 100  
         print("Sound Level:", sound_level)
     else:
         print("Failed to read the sound level values.")
+    time.sleep(2)
 
 def stop_measurement():
-    # Send a POST request to stop the measurement
-    response = requests.post(host + "/webxi/applications/slm/stop")
+    response = requests.post(host + "/webxi/applications/SLM/stop?password=xxxxxxx")
     if response.status_code == 200:
         print("Measurement stopped successfully.")
+    elif response.status_code == 400:
+        print("Bad Request. Status code:", response.status_code)
+    elif response.status_code == 500:
+        print("Internal Server Error. Status code:", response.status_code)
     else:
-        print("Failed to stop the measurement.")
+        print("Failed to stop the measurement. Status code:", response.status_code)
+    time.sleep(2)
 
-# Main application loop
 while True:
-    # Display menu options
     print("Menu:")
     print("1. Start Measurement")
     print("2. Read Values")
